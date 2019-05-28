92c4b1ac6dba7adf119286d6d4c7663f8ef8e620
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-05-27 22:30:03 -0500

    Botones Page

--- disenos/lib/main.dart
+++ disenos/lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:disenos/src/pages/basico_page.dart';
+import 'package:disenos/src/pages/botones_page.dart';
 import 'package:disenos/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.transparent,
+    ));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basico': (context) => BasicoPage(),
-        'scroll': (context) => ScrollPage()
+        'scroll': (context) => ScrollPage(),
+        'botones': (context) => BotonesPage(),
       },
     );
   }
