285583af0bb946ba802d46f37488c8a291f3e543
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-05-28 22:23:20 -0500

    Added QRReader, Scan Model and Scan sqlite provider

---
+++ qr_reader_app/lib/src/pages/home_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader_app/src/pages/direcciones_page.dart';
+import 'package:qr_reader_app/src/pages/mapas_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: _scanQR,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  _scanQR() async {
+    // http://google.com
+    // geo:40.72111069314909,-73.75050917109377
+
+    String futureString = '';
+
+    /*try {
+      futureString = await QRCodeReader().scan();
+    } catch (e) {
+      futureString = e.toString();
+    }
+
+    print(futureString);
+
+    if (futureString != null) {}*/
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+    }
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones'),
+        )
+      ],
+    );
+  }
+}
